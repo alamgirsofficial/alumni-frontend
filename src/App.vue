dde460c1c5c40c98735124b143d01e61efb998a7
Md. Alamgir <alamgirbahadur.bd@gmail.com>
2026-08-03 23:56:07 +0600

    This is Folder Struture for Forum Management System with VueJS and Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+
+import Header from './components/layouts/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+
+
+
+</script>
+
+<template>
+
+  <Header />
+
+
+
+</template>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2 class="text-3xl font-bold underline"> This is from Header component</h2>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
